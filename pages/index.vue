f5a53a13ef6cf2f20da7b46bcb1b0f8f0d56e5d1
Alejandro Martos <alexmartos96@gmail.com>
2022-03-28 22:00:26 -0700

    changing the video

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <video width="100%" height="100%" autoplay playsinline muted loop>
-      <source src="/landingvideo.mp4" type="video/mp4" />
+      <source src="/realvideo.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     <b-container fluid>
