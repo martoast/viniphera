9c8c8b5fc8ab160d378731d8a005543495382d6d
Alejandro Martos <alexmartos96@gmail.com>
2022-04-10 14:55:26 -0700

    shit

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,10 @@
       Your browser does not support the video tag.
     </video>
     <b-container class="py-5">
-      <h1 class="text-center" style="text-align: center; font-family: 'Athiti'">
+      <h1
+        class="text-center"
+        style="text-align: center; font-family: 'Athiti'; color: purple"
+      >
         From Nature To Your Skin
       </h1>
       <h3 class="text-center" style="text-align: center; font-family: 'Athiti'">
@@ -14,31 +17,78 @@
         region; environmentally friendly and not tested on animals.
       </h3>
     </b-container>
+    <b-container class="text-center py-5" >
+         
+         <b-row align-h="center"  align-v="center" no-gutters>
+            <b-col class="py-5" md="4" cols="12">
+               
+               <b-row  no-gutters>
+                     <b-col>
+                        
+                     <img style="max-width: 50%;"  class="mx-2" src="/noanimaltesting.png"></img>
+                  
+                        <div style="font-family: 'Athiti'; font-size: 28px;" class="font-weight-bold text-gray">Not Tested On Animals</div>
+                        <div style="font-family: 'Athiti'; font-size: 22px;">We stronly believe in producing a "cruilty free" soap that is safe for all types of skin. </div>
+                     </b-col>
+                  </b-row>
+            </b-col>
+            <b-col class="py-5" md="4" cols="12">
+               <div style="align-items: center; justify-content: center;">
+                 
+                  <b-row  no-gutters>
+                     <b-col>
+                       
+                     <img style="max-width: 55%;" class="mx-2" src="/sulfatefree.png"></img>
+                 
+                        <div style="font-family: 'Athiti'; font-size: 28px;" class="font-weight-bold text-gray">Free of Sulfates and Prabens</div>
+                        <div style="font-family: 'Athiti'; font-size: 22px;">Our naturally produced soaps provide your skin the nourishment and moisturising it decerves with out the need of added artificial chemicals.</div>
+                     </b-col>
+                  </b-row>
+               </div>
+            </b-col>
+            <b-col class="py-5" md="4" cols="12">
+               <div style="align-items: center; justify-content: center;">
+                  
+                  <b-row  no-gutters>
+                     <b-col>
+                       
+                           <img style="max-width: 50%;" class="mx-2" src="/madeinmexico.jpeg"></img>
+                 
+                        <div style="font-family: 'Athiti'; font-size: 28px;">100% Made in Mexico</div>
+                        <div style="font-family: 'Athiti'; font-size: 22px;">Produced in Ensenada Baja California with locally sourced ingredients. </div>
+                     </b-col>
+                  </b-row>
+               </div>
+            </b-col>
+         </b-row>
+      </b-container>
     <b-container class="py-5">
       <b-row align-h="center" align-v="center">
         <b-col class="py-5" cols="12" md="6" sm="12">
-          <h2 class="text-center py-3" style="font-family: 'Athiti'">
-            A new kind of soap
+          <h2
+            class="text-center py-3"
+            style="font-family: 'Athiti'; color: purple"
+          >
+            A New Kind Of Soap
           </h2>
+          <p>
+            Thanks to the fact that grape seed oil contains a hundred times more
+            polyphenols and antioxidants than vitamin E and is so rich in
+            tannins. Our soap protects the fibers of the skin, detoxifies
+            tissues and improves circulation, helping reduce skin ageing marks
+            while providing a nourishing sking hydration to will keep your skin
+            healthy.
+          </p>
+
           <p>
             The name of Viníphera comes from one of the main ingredients in our
             soaps, the vitis vinifera. We were inspired to change the letter "F"
-            for "PH" since our soaps proudly contain a neutral pH.
-          </p>
-          <p>
-            Innovative technique that deeply hydrates and nourishes the skin,
-            thanks to the fact that grape seed oil contains a hundred times more
-            polyphenols and antioxidants than vitamin E, it is also rich in
-            tannins that contribute to skin rejuvenation, protects the fibers of
-            the skin support, detoxifies tissues and improves circulation, helps
-            reduce skin marks and expression lines, tone and firm the skin
-            contributing to the youth of the body, keeping it healthy and
-            therefore beautiful.
+            for "PH" because our soaps proudly contain a neutral pH.
           </p>
         </b-col>
         <b-col cols="12" md="6" sm="12">
           <b-card
-            img-src="/logo-hoja.png"
+            img-src="/together.png"
             style="max-width: 40rem"
             img-alt="Image"
             class="border-0"
@@ -46,6 +96,52 @@
           ></b-card>
         </b-col>
       </b-row>
+    </b-container>
+    <b-container class="py-5">
+      <b-card-group class="d-none d-md-block" columns>
+               <b-card
+                  img-src="/winesoap2.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+                  <b-card
+                  img-src="/olivesoap.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+                  <b-card
+                  img-src="/mintsoap.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+                  <b-card
+                  img-src="/charcoalsoap.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+                  <b-card
+                  img-src="/oatmealmilk.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+                  <b-card
+                  img-src="/lavandersoap.png"
+                  img-alt="Image"
+                  class="border-0"
+                  style="cursor: pointer;"
+                  overlay
+                  ></b-card>
+      </b-card-group>
     </b-container>
     <b-container class="py-5">
       <b-row align-v="center">
@@ -58,10 +154,7 @@
           ></b-card>
         </b-col>
         <b-col cols="12" md="6" sm="12">
-          <h1
-            class="text-primary"
-            style="text-align: center; font-family: 'Allura'"
-          >
+          <h1 style="text-align: center; font-family: 'Allura'; color: purple">
             Alejandra Correa Terán.
           </h1>
           <h3
@@ -82,6 +175,48 @@
         </b-col>
       </b-row>
     </b-container>
+    <b-container fluid class="bg-primary text-center">
+         <b-row class="p-4" align-v="center" align-h="center">
+            <b-col cols="12" md="auto" sm="12" class="mr-auto p-3 px-md-5">
+               <div class="d-block d-md-none py-3">
+                  <b-btn class="p-0" href="//twitter.com/theheistworld" variant="link">
+                     <img style="width: 2rem;" class="mx-2" src="/mobile/twittermobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//discord.com/invite/theheistworld" variant="link">
+                     <img style="width: 3rem;" class="mx-1" src="/mobile/discordmobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//opensea.io/collection/the-heist-collection" variant="link">
+                     <img style="max-width: 3rem;" class="mx-2" src="/mobile/openseamobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//etherscan.io/address/0xcF8009F8E3446296d02B63d43E8066D130ce27B3" variant="link">
+                     <img style="max-width: 2rem;" class="mx-2" src="/mobile/etherscanmobile.svg"></img>
+                  </b-btn>
+               </div>
+               
+               <div>2022 Viníphera ™</div>
+            </b-col>
+            <b-col cols="12" md="auto" sm="12" class="ml-auto "
+               >
+
+               <div class="d-none d-md-block">
+                 <b-btn class="p-0" href="//twitter.com/theheistworld" variant="link">
+                     <img style="width: 2rem;" class="mx-2" src="/mobile/twittermobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//discord.com/invite/theheistworld" variant="link">
+                     <img style="width: 3rem;" class="mx-1" src="/mobile/discordmobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//opensea.io/collection/the-heist-collection" variant="link">
+                     <img style="max-width: 3rem;" class="mx-2" src="/mobile/openseamobile.svg"></img>
+                  </b-btn>
+                  <b-btn class="p-0" href="//etherscan.io/address/0xcF8009F8E3446296d02B63d43E8066D130ce27B3" variant="link">
+                     <img style="max-width: 2rem;" class="mx-2" src="/mobile/etherscanmobile.svg"></img>
+                  </b-btn>
+               </div>
+                
+               
+            </b-col>
+         </b-row>
+      </b-container>
   </div>
 </template>
 
